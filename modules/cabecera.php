757dfa54d0b5611a79c8c29d45afba361323a49c
RAMIRO LEONARDO RAMIREZ CORONEL <rlramirez@utpl.edu.ec>
2019-04-25 18:35:00 -0500

    cambios de la clase y gitinore

--- modules/cabecera.php
+++ modules/cabecera.php
@@ -1,20 +1,24 @@
+<?php
+ include ("../dll/config.php");
+?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>VIsualizacion de datos de aves</title>
+	<title><?php echo $site_name;?></title>
 	<meta charset="utf-8">
 	<meta name="description" content="Visualización de aves del Ecuador">
 	<meta name="keywords" charset="aves, ecuador, turismo, loja, bird">
 	<meta name="author" content="UTPL by @rlramirez">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo $site_url;?> /css/estilos.css">
 	<link href='http://fonts.googleapis.com/css?family=Josefin+Slab' rel='stylesheet' type='text/css' />
 
 </head>
 <body>
 	<header>
 		<h1>Bird Visualizations <span class="slogan">with HTML5 and CSS3</span></h1>
+		<?php echo $site_name;?>
 		<nav>
 			<a href="" class="menuinicio">Home</a>
 			<a href="#databases">Aves</a>
