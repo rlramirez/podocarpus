8a29f076b4764d6523a9cbe279299f1288c8f661
Yoder <ybaguirre@utpl.edu.ec>
2019-05-15 17:24:27 -0500

    Sensores
    Actualizacion de sensores

---
+++ sensores.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 13-05-2019 a las 05:41:59
+-- Versión del servidor: 10.1.38-MariaDB
+-- Versión de PHP: 7.3.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `podocarpusdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sensores`
+--
+
+CREATE TABLE `sensores` (
+  `id_sensor` int(11) NOT NULL,
+  `latitud` varchar(24) NOT NULL,
+  `longitud` varchar(24) NOT NULL,
+  `descripcion` varchar(300) NOT NULL,
+  `tipo_dato` varchar(10) NOT NULL,
+  `extraccion_dato` varchar(20) NOT NULL,
+  `nombre_sensor` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `sensores`
+--
+
+INSERT INTO `sensores` (`id_sensor`, `latitud`, `longitud`, `descripcion`, `tipo_dato`, `extraccion_dato`, `nombre_sensor`) VALUES
+(1, ' -4,3571', '-78,974', 'esta cámara se encuentra al sur del parque', 'jpg', 'manual', 'camara digital ');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
