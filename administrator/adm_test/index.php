cd5cdd5c4e101f7007f3dc33fa2f9d1fc16ae3cb
mayueliza92 <mayueliza1992@gmail.com>
2019-05-22 18:04:42 -0500

    prueba de web por margory Ayala

--- administrator/adm_test/index.php
+++ administrator/adm_test/index.php
@@ -10,6 +10,10 @@
 		<h3>Nombre y Apellido</h3>
 		<p><a href="ramiro_ramirez/index.php">Ver Examen</a></p>
 	</section>
+		<section class="box-dashboard c4">
+		<h3>Margory Ayala</h3>
+		<p><a href="margory_ayala/index.php">Ver Examen</a></p>
+	</section>
 </main>
 <?php
 	include('../adm_footer.php');
