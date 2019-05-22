44957abd477ff2dd69ce953a7e26e44bccb92a8b
Juan Pablo Pachar Viñan <jpablopachar1993@gmail.com>
2019-05-22 17:44:54 -0500

    Examen primer bimestre Juan Pablo Pachar

--- administrator/adm_test/index.php
+++ administrator/adm_test/index.php
@@ -10,6 +10,10 @@
 		<h3>Nombre y Apellido</h3>
 		<p><a href="ramiro_ramirez/index.php">Ver Examen</a></p>
 	</section>
+    <section class="box-dashboard c4">
+        <h3>Juan Pablo Pachar Viñan</h3>
+        <p><a href="juan_pachar/index.php">Ver Examen</a></p>
+    </section>
 </main>
 <?php
 	include('../adm_footer.php');
--- podocarpusdb.sql
+++ podocarpusdb.sql
@@ -209,3 +209,16 @@ INSERT INTO `podocarpusdb`.`lugares` (`idLugares`, `nombre`, `latitud`, `longitu
 -- -----------------------------------------------------
 INSERT INTO `podocarpusdb`.`fotos` (`idFotos`, `url`, `id_lugares`) VALUES (1, 'https://especiales.elcomercio.com/planeta-ideas/imagenes/imagenes/ambiente-laguna.jpg', 1);
 
+CREATE TABLE `pachar_sensores` (
+  `idSensores` int(11) NOT NULL,
+  `temperatura` VARCHAR(10)DEFAULT NULL,
+  `hora` VARCHAR(10) DEFAULT NULL
+  `fecha` varchar(100) DEFAULT NULL,
+  `anemometro` VARCHAR(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `pachar_sensore` (`idSensores`, `temperatura`, `hora`, `fecha`, `anemometro`) VALUES (1, '43', '09:00', '2019-05-30', '345');
+INSERT INTO `pachar_sensore` (`idSensores`, `temperatura`, `hora`, `fecha`, `anemometro`) VALUES (2, '43', '10:00', '2015-04-05', '234');
+INSERT INTO `pachar_sensore` (`idSensores`, `temperatura`, `hora`, `fecha`, `anemometro`) VALUES (6, '43', '19:00', '2019-07-22', '275');
+INSERT INTO `pachar_sensore` (`idSensores`, `temperatura`, `hora`, `fecha`, `anemometro`) VALUES (7, '43', '08:00', '2019-05-20', '30');
+INSERT INTO `pachar_sensore` (`idSensores`, `temperatura`, `hora`, `fecha`, `anemometro`) VALUES (8, '43', '18:00', '2019-07-20', '55');
